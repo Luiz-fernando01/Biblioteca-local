a160fcbaf1a00e9b5e5e47a7583226268fb2e080
Luiz-fernando01 <selauadriana@gmail.com>
2023-07-28 14:04:04 -0300

    adição de modulos e rotas novas

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/cool', function(req, res, next) {
+  res.render('index', { title: 'you are so cool' });
+});
+
+router.get('/cool/PC', function(req, res, next) {
+  res.send('É os guri da TI' );
+});
+
+router.get('/ifc', function(req, res, next) {
+  res.render('index', { title: 'bem vindo ao IFC' });
+});
+
+
 module.exports = router;
